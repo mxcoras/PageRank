88caf9b4df244c6d7a45863883855639f6226e28
MxEmerson <2382413024@qq.com>
2021-04-10 02:06:54 +0800

    fix: 修正了累加时的bug，完善了注释

--- generator.py
+++ generator.py
@@ -1,32 +1,45 @@
 import random
 
 
-def generator(N: int) -> list:
-    """
-    生成大小为N*N的迁移矩阵，
-    以三元组表示并分块，
-    将所有分块使用嵌套列表的方式返回
+def generator(N: int, step: int) -> list:
+    """生成大小为N*N的迁移矩阵，以三元组表示并分块
+
+    Args: 
+        N: 转移矩阵尺寸，大于15
+        step: 每个分块的大小，必须分为至少两块
+
+    Returns:
+        分块的迁移矩阵，返回类型为list。
+    
     """
     matrix_num = [i for i in range(N)]  # 用于加速列表推导式
-    XofC = [random.randint(6, 15) for i in range(N)]
-    matrix = [(i, XofC[i], sorted(random.sample(matrix_num, XofC[i])))
+    degree = [random.randint(6, 15) for i in range(N)]
+    matrix = [(i, degree[i], sorted(random.sample(matrix_num, degree[i])))
               for i in range(N)]
-    par_siz = 100
-    par_num = N // par_siz
-    par = [[] for i in range(par_num)]
+    """
+    没有节点指向自己的情况：
+    matrix = []
+    for i in range(N):
+        matrix_num.remove(i)
+        matrix.append((i, degree[i], sorted(random.sample(matrix_num, degree[i]))))
+        matrix_num.append(i) 
+    """
+    part_size = step
+    part_num = N // part_size
+    part = [[] for i in range(part_num)]
     for m in matrix:
         left = 0
-        old_seq = m[2][0] // par_siz
+        old_seq = m[2][0] // part_size
         for i in range(len(m[2])):
-            new_seq = m[2][i] // par_siz
+            new_seq = m[2][i] // part_size
             if old_seq != new_seq:
                 new_m = (m[0], m[1], m[2][left:i])
-                par[old_seq].append(new_m)
-                if new_seq == par_num - 1:
+                part[old_seq].append(new_m)
+                if new_seq == part_num - 1:
                     new_m = (m[0], m[1], m[2][i:])
-                    par[new_seq].append(new_m)
+                    part[new_seq].append(new_m)
                     break
                 else:
                     left = i
                     old_seq = new_seq
-    return par
+    return part
