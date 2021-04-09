e6ec254ad66aafb5b5e29e4cfedc3deb3b4fbf27
MxEmerson <2382413024@qq.com>
2021-04-09 16:56:26 +0800

    perf: 完善生成算法，提高性能

--- generator.py
+++ generator.py
@@ -1,55 +1,40 @@
-from random import randint
+import random
 
-def generator(N:int) -> list:
+
+def generator(N: int) -> list:
     """
     生成大小为N*N的迁移矩阵，以嵌套列表的方式返回
     注意不能使用numpy
     """
+    matrix_num = [i for i in range(N)] # 用于加速列表推导式
+    XofC = [random.randint(6, 15) for i in range(N)]
+    matrix = [(i, XofC[i], random.sample(matrix_num, XofC[i]))
+              for i in range(N)]
     # TODO
-    pass
+    # 将matrix分块，以一个列表的形式返回
+    # return [M1, M2, M3, ...]
 
-import random
+
+"""
+生成原始矩阵
 
 N = eval(input("请输入行数(行数不得少于15)： "))
 
-matrix = []
-for i in range(N):
-    matrix.append(0)
+matrix = [0 for i in range(N)]
 
-#查阅过资料，random库中的随机数分布就是均匀分布
+# 查阅过资料，random库中的随机数分布就是均匀分布
 
-XofC = random.randint(6,15)
+XofC = random.randint(6, 15)
 print("随机数d为：{}".format(XofC))
 
 matrix_num = []
 for j in range(N):
     matrix_num.append(j)
 
-rand = random.sample(matrix_num,XofC)
+rand = random.sample(matrix_num, XofC)
 
 for k in rand:
-    matrix[k]=(1/XofC)
+    matrix[k] = (1/XofC)
 
 print(matrix)
-
-
-############################################################
-
-import random
-
-N = eval(input("请输入节点数： "))
-matrix = []
-matrix_num = []
-for j in range(N):
-    matrix_num.append(j)
-#查阅过资料，random库中的随机数分布就是均匀分布
-
-for i in range(N):
-    matrix_n = []
-    XofC = random.randint(6,15)
-    rand = random.sample(matrix_num,XofC)
-    node_n = (i,XofC,rand)
-    matrix.append(node_n)
-
-print(matrix)
-
+"""
--- pagerank.py
+++ pagerank.py
@@ -1,67 +1,3 @@
-def PageRank():
-    # TODO
-    pass
-
-class Matrix:
-    """
-    封装了矩阵乘法，包括矩阵乘矩阵，数乘矩阵
-    """
-    def __init__(self, row, col) -> None:
-        self.row = row
-        self.col = col
-        self.A = [[0 for i in range(col)] for j in range(row)]
-
-    def value(self, value:list):
-        if len(value) == self.row and len(value[0]) == self.col:
-            self.A = value
-
-    def __getitem__(self, i):
-        return self.A[i]
-
-    def __mul__(self, B):
-        """
-        重载乘法运算符，用于矩阵乘矩阵
-        """
-        if self.col != B.row:
-            return Matrix(1, 1)
-        C = Matrix(self.row, B.col)
-        for i in range(C.row):
-            for j in range(C.col):
-                for p in range(B.row):
-                    C[i][j] += self.A[i][p] * B[p][j]
-        return C
-
-    def __rmul__(self, B):
-        """
-        反向重载乘法运算符，用于数乘矩阵
-        """
-        for i in range(self.row):
-            self.A[i] = list(map(lambda x: x*B, self.A[i]))
-        return self
-
-    def print(self):
-        print(self.A)
-
-"""
-使用示例
-A = Matrix(4, 3)
-B = Matrix(3, 2)
-A1 = [[5, 2, 4], [3, 8, 2], [6, 0, 4], [0, 1, 6]]
-B1 = [[2, 4], [1, 3], [3, 2]]
-A.value(A1)
-B.value(B1)
-C = A*B
-C.print()
-D = 2*A
-D.print()
-"""
-
-"""
-稀疏矩阵的数据结构（未分块）
-[(src, degree, [dest, ...]),
-(src, degree, [dest, ...]),
-(src, degree, [dest, ...])]
-"""
 import math
 
 def error(rank:list, last:list)->float:
@@ -71,6 +7,19 @@ def error(rank:list, last:list)->float:
     mysum = math.sqrt(mysum)
     print(1)
     return mysum
+
+def PageRank():
+    # TODO
+    pass
+
+"""
+稀疏矩阵的数据结构（未分块）
+[(src, degree, [dest, ...]),
+(src, degree, [dest, ...]),
+(src, degree, [dest, ...])]
+"""
+
+# PageRank算法demo
 
 M1 = [
     (0, 4, [0, 1]),
