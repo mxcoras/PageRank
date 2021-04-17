5f2607eb13aa5dcf0c5bd750f6db2639d9c7caf2
MxEmerson <2382413024@qq.com>
2021-04-17 22:26:03 +0800

    perf: 分块存储矩阵

--- lib/generator.py
+++ lib/generator.py
@@ -3,10 +3,12 @@ import random
 from time import perf_counter
 
 
-def generator(N: int, part_size: int):
+def generator(L: int, R: int, N: int, part_size: int):
     """生成大小为N*N的迁移矩阵，以三元组表示并分块
 
     Args:
+        L: 生成起始位置
+        R: 生成结束位置
         N: 转移矩阵尺寸，大于15
         part_size: 每个分块的大小，必须分为至少两块
 
@@ -15,16 +17,35 @@ def generator(N: int, part_size: int):
         格式为[[(src, degree, [dest, ...]), ...], ...]
     
     """
+    table = L // part_size
     t = perf_counter()
-    conn = sqlite3.connect("generator.db")
+    # conn = mysql.connector.connect(user='root', password='123456', database='generator')
+    conn = sqlite3.connect(f"db/generator{table}.db")
     c = conn.cursor()
-    matrix_num = [i for i in range(N)]  # 用于加速列表推导式
-    degree = [random.randint(6, 15) for i in range(N)]
+    c.execute(f"""create table part
+                (part_num int not null,
+                src int not null,
+                degree int not null,
+                dest text not null,
+                primary key(part_num, src));""")
+    conn.commit()
+    # matrix_num = [i for i in range(N)]  # 用于加速列表推导式
+    # degree = [random.randint(6, 15) for i in range(N)]
     part_num = N // part_size
-    for k in range(N):
-        if k % 100000 == 0:
-            print(k)
-        m = (k, degree[k], sorted(random.sample(matrix_num, degree[k])))
+    # conn2 = sqlite3.connect(f"db/degree{table}.db")
+    # c2 = conn2.cursor()
+    for k in range(L, R):
+        # if k % 1000000 == 0:
+        #     print(k)
+        # degree = c2.execute(f"select deg from degree where id = {k}").fetchone()[0]
+        numbers = []
+        degree = random.randint(6, 15)
+        for i in range(degree):
+            num = -1
+            while num in numbers or num == -1:
+                num = random.randrange(0, N)
+            numbers.append(num)
+        m = (k, degree, sorted(numbers))
         left = 0
         old_seq = m[2][0] // part_size
         for i in range(len(m[2])):
@@ -41,6 +62,6 @@ def generator(N: int, part_size: int):
                     old_seq = new_seq
     conn.commit()
     conn.close()
-    print(f"矩阵生成用时：{perf_counter() - t}秒。")
+    # print(f"矩阵生成用时：{perf_counter() - t}秒。")
     # print(f"原始稀疏矩阵占用内存：{sys.getsizeof(matrix)/1024}KB")
     # print(f"三元组分块表示的稀疏矩阵占用内存：{sys.getsizeof(part)/1024}KB")
--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-import sqlite3
+# import random
+# import sqlite3
+from time import perf_counter
+from multiprocessing import Process
 from bin.pagerank import PageRank
+from lib.generator import generator
 
+
+# def degree(L, R):
+#     conn = sqlite3.connect(f"db/degree{L // 10000000}.db")
+#     c = conn.cursor()
+#     c.execute(f"""create table degree
+#                 (id int primary key not null,
+#                 deg int not null);""")
+#     for i in range(L, R):
+#         c.execute(f"insert into degree (id, deg) values ({i}, {random.randint(6, 15)})")
 
 def main():
-    conn = sqlite3.connect("C:/Users/MxCorAS/Documents/DB/generator.db")
-    c = conn.cursor()
-    c.execute(f"""create table part
-                (part_num int not null,
-                src int not null,
-                degree int not null,
-                dest text default '[]' not null,
-                primary key(part_num, src));""")
-    conn.commit()
-    conn.close()
-    myrank = list(enumerate(PageRank(10000000, 10000)))
+    N = 1000000
+    part_count = 10
+    # t = perf_counter()
+    # ps = []
+    # for i in range(part_count):
+    #     p = Process(target=degree, args=(i*(N//part_count), (i+1)*(N//part_count)))
+    #     ps.append(p)
+    # for i in range(part_count):
+    #     ps[i].start()
+    # for i in range(part_count):
+    #     ps[i].join()
+    # degree = [random.randint(6, 15) for i in range(N)]
+    # print(f"degree生成用时：{perf_counter() - t}秒。")
+    t = perf_counter()
+    ps = []
+    for i in range(part_count):
+        p = Process(target=generator, args=(i*(N//part_count), (i+1)*(N//part_count), N, (N//part_count)))
+        ps.append(p)
+    for i in range(part_count):
+        ps[i].start()
+    for i in range(part_count):
+        ps[i].join()
+    print(f"矩阵生成用时：{perf_counter() - t}秒。")
+    myrank = list(enumerate(PageRank(N, N//part_count)))
     myrank.sort(key=lambda x: x[1], reverse=True)
     print("排名\t编号\tPageRank值")
     for i in range(10):
